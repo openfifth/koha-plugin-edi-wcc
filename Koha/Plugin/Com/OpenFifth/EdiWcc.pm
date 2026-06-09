e19a69dd4672ee5d0502c751c8832c78eed5a1e0
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-06-09 14:12:17 +0100

    chore: bump version

--- Koha/Plugin/Com/OpenFifth/EdiWcc.pm
+++ Koha/Plugin/Com/OpenFifth/EdiWcc.pm
@@ -10,7 +10,7 @@ use Koha::Logger;
 use Koha::Token;
 use JSON qw( decode_json encode_json );
 
-our $VERSION = '0.1.4';
+our $VERSION = '0.1.5';
 
 our $metadata = {
     name            => 'EDI Service Charges (WCC)',
